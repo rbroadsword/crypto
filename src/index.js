999493803dd649123824d9e12489c9fddb985143
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-29 11:40:31 -0700

    add key to env file and template literal to url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-
+const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids= 5DAI,BTC,UNI,ETH,ZRX,BAT,ADA,HEX,LTC,1INCH,DOGE,XRP,XLM,USDT,USDC&currency=EUR&interval=1d,7d,30d,365d,ytd&per-page=100&page=1`
 $(document).ready(function() {
 
 }); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack'); 
 
 module.exports = {
   entry: './src/index.js',
@@ -18,7 +19,8 @@ module.exports = {
       title: 'template',      // 
       template: './src/index.html',
       inject: 'body'
-    })
+    }), 
+    new Dotenv()
   ],
   module: {
     rules: [
@@ -29,7 +31,8 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/img/'
+              outputPath: 'assets/img/', 
+              esModule: false
             }
           }
         ]
